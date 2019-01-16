08ddff32d184117454be22d5c3bfa2b3dc33ade1
Ainslie Vaux <ainslie.vaux@gmail.com>
2019-01-15 21:35:58 -0500

    Fix stuff in Durotar

---
+++ Updates/2281_fix_quest_823.sql
@@ -0,0 +1,1 @@
+UPDATE dbscript_string SET content_default='%s listens to $n\'s report...' WHERE entry=2000005777;
---
+++ Updates/2282_ooze_covered_gold_vein_durotar.sql
@@ -0,0 +1,2 @@
+/* Remove Ooze Covered Gold Vein spawning in Durotar */
+DELETE FROM gameobject WHERE guid=14685;
