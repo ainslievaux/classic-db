16493e3e39b61113077457c86326bf1c54653fff
Ainslie Vaux <ainslie.vaux@gmail.com>
2019-01-24 17:11:26 -0500

    Fix creatures dropping many items they should not

---
+++ Updates/9995_creature_drops.sql
@@ -0,0 +1,62 @@
+/* Herbs */
+DELETE FROM creature_loot_template WHERE item IN (
+8836,  /* Arthas' Tears */
+13468, /* Black Lotus */
+8839,  /* Blindweed */
+2450,  /* Briarthorn */
+2453,  /* Bruiseweed */
+13463, /* Dreamfoil */
+2449,  /* Earthroot */
+3818,  /* Fadeleaf */
+4625,  /* Firebloom */
+8845,  /* Ghost Mushroom */
+13464, /* Golden Sansam */
+3821,  /* Goldthorn */
+3369,  /* Grave Moss */
+8846,  /* Gromsblood */
+13467, /* Icecap */
+3358,  /* Khadgar's Whisker */
+3356,  /* Kingsblood */
+3357,  /* Liferoot */
+785,   /* Mageroyal */
+13465, /* Mountain Silversage */
+2447,  /* Peacebloom */
+13466, /* Plaguebloom */
+8831,  /* Purple Lotus */
+765,   /* Silverleaf */
+3820,  /* Stranglekelp */
+8838,  /* Sungrass */
+2452,  /* Swiftthistle */
+3355,  /* Wild Steelbloom */
+3819   /* Wintersbite */
+);
+
+/* Ores */
+DELETE FROM creature_loot_template WHERE item IN (
+2770,  /* Copper Ore */
+2776,  /* Gold Ore */
+2772,  /* Iron Ore */
+3858,  /* Mithril Ore */
+2775,  /* Silver Ore */
+10620, /* Thorium Ore */
+2771,  /* Tin Ore */
+7911   /* Truesilver Ore */
+);
+
+/* Stones */
+DELETE FROM creature_loot_template WHERE item IN (
+2836,  /* Coarse Stone */
+12365, /* Dense Stone */
+2838,  /* Heavy Stone */
+2835,  /* Rough Stone */
+7912   /* Solid Stone */
+);
+
+/* Bolts of Cloth */
+DELETE FROM creature_loot_template WHERE item IN (
+2996,  /* Bolt of Linen Cloth */
+4339,  /* Bolt of Mageweave */
+14048, /* Bolt of Runecloth */
+4305,  /* Bolt of Silk Cloth */
+2997   /* Bolt of Woolen Cloth */
+);
--- Updates/9996_increase_crawler_spawns.sql
+++ Updates/9996_increase_crawler_spawns.sql
@@ -1,5 +1,5 @@
-INSERT INTO creature (id, map, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecsmin, spawntimesecsmax, currentwaypoint, curhealth, curmana, deathstate, movementtype) VALUES
-(830, 0, 0, 0, -9666.19, 1376.14, 15.292,  6.07384, 25, 25, 5, 0, 273, 0, 0, 1),
-(830, 0, 0, 0, -9660.85, 1311.86, 9.10265, 1.12191, 25, 25, 5, 0, 273, 0, 0, 1),
-(830, 0, 0, 0, -9645.65, 1293.66, 6.0813,  5.94425, 25, 25, 5, 0, 300, 0, 0, 1),
-(830, 0, 0, 0, -9671.93, 1291.48, 9.06262, 2.50185, 25, 25, 5, 0, 273, 0, 0, 1);
+INSERT INTO creature (id, map, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecsmin, spawntimesecsmax, spawndist, currentwaypoint, curhealth, curmana, deathstate, movementtype) VALUES
+(830, 0, 0, 0, -9666.19, 1376.14, 15.292,  6.07384, 300, 300, 10, 0, 273, 0, 0, 1),
+(830, 0, 0, 0, -9660.85, 1311.86, 9.10265, 1.12191, 300, 300, 10, 0, 273, 0, 0, 1),
+(830, 0, 0, 0, -9645.65, 1293.66, 6.0813,  5.94425, 300, 300, 10, 0, 273, 0, 0, 1),
+(830, 0, 0, 0, -9671.93, 1291.48, 9.06262, 2.50185, 300, 300, 10, 0, 273, 0, 0, 1);
