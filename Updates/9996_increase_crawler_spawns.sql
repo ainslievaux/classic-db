cb529cefff17a3ee0abf45e2f9c186b242ebc056
Ainslie Vaux <ainslie.vaux@gmail.com>
2019-01-28 16:47:07 -0500

    Increase the number of all types of crawlers in Westfall

--- Updates/9996_increase_crawler_spawns.sql
+++ Updates/9996_increase_crawler_spawns.sql
@@ -1,5 +1,17 @@
 INSERT INTO creature (id, map, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecsmin, spawntimesecsmax, spawndist, currentwaypoint, curhealth, curmana, deathstate, movementtype) VALUES
+/* Sand Crawler */
 (830, 0, 0, 0, -9666.19, 1376.14, 15.292,  6.07384, 300, 300, 10, 0, 273, 0, 0, 1),
 (830, 0, 0, 0, -9660.85, 1311.86, 9.10265, 1.12191, 300, 300, 10, 0, 273, 0, 0, 1),
 (830, 0, 0, 0, -9645.65, 1293.66, 6.0813,  5.94425, 300, 300, 10, 0, 273, 0, 0, 1),
 (830, 0, 0, 0, -9671.93, 1291.48, 9.06262, 2.50185, 300, 300, 10, 0, 273, 0, 0, 1);
+/* Sea Crawler */
+INSERT INTO CREATURE (id, map, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecsmin, spawntimesecsmax, spawndist, currentwaypoint, curhealth, curmana, deathstate, movementtype) VALUES
+(831, 0, 0, 0, -9937.19, 1892.43, 4.03539, 0.0541911, 300, 300, 5, 0, 328, 0, 0, 1), 
+(831, 0, 0, 0, -9960.26, 1886.5, 9.25272, 2.81173, 300, 300, 5, 0, 356, 0, 0, 1), 
+(831, 0, 0, 0, -9944.71, 1869.19, 9.64721, 2.42296, 300, 300, 5, 0, 328, 0, 0, 1), 
+(831, 0, 0, 0, -9908.38, 1892.27, -1.36789, 1.24799, 300, 300, 5, 0, 356, 0, 0, 1);
+/* Shore Crawler */
+INSERT INTO CREATURE (id, map, modelid, equipment_id, position_x, position_y, position_z, orientation, spawntimesecsmin, spawntimesecsmax, spawndist, currentwaypoint, curhealth, curmana, deathstate, movementtype) VALUES
+(1216, 0, 0, 0, -10920.3, 2097.78, 9.54381, 3.6788, 300, 300, 5, 0, 386, 0, 0, 1),
+(1216, 0, 0, 0, -10935.4, 2085.65, 10.6068, 3.8398, 300, 300, 5, 0, 386, 0, 0, 1)
+
